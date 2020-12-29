f840d4dcf228b1d3a58b365004648f7e8af120b7
Sandeep Singh <sandeepgaurdec13@gmail.com>
2020-12-29 15:02:18 +0530

    Check Employee is present or not

---
+++ CheckEmployee.java
@@ -0,0 +1,17 @@
+/**
+ * author Sandeep_Singh
+ * Function Check Employee is present or not
+ *
+ */
+public class CheckEmployee
+{
+	private static final int is_present=1;
+    public static void main( String[] args )
+    {    
+    	double check_employee=Math.floor(Math.random()*10)%2;
+    	if(check_employee==is_present)
+        System.out.println( "Employee is present");
+    	else
+    		System.out.println("Employee is not present");
+    }
+}
