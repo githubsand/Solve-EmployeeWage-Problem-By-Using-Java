93eb6b6470398353186fe81d52d89974e8ead97d
Sandeep Singh <sandeepgaurdec13@gmail.com>
2020-12-29 14:45:14 +0530

    This is a Main Branch

---
+++ EmployeeWage.java
@@ -0,0 +1,15 @@
+/**
+ * author Sandeep_Singh
+ * Function Simply print Welcome Message
+ *
+ */
+public class EmployeeWage
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to  EmployeeWage Computation!" );
+    }
+}
+
+
+
